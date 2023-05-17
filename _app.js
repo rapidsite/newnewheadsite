023f12f49d596d12ae56b9ad79ea582431ddcf9d
Rapidsite <128565288+rapidsite@users.noreply.github.com>
2023-05-17 09:02:46 -0500

    Add files via upload

---
+++ _app.js
@@ -0,0 +1,36 @@
+import Aos from "aos";
+import { useEffect } from "react";
+import "aos/dist/aos.css";
+import "../styles/index.scss";
+import ScrollToTop from "../components/common/ScrollTop";
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
+
+if (typeof window !== "undefined") {
+  require("bootstrap/dist/js/bootstrap");
+}
+
+function MyApp ({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(0123456, 1)
+  }, [])
+return <Component {...pageProps} />
+}
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    Aos.init({
+      duration: 1200,
+    });
+  }, []);
+  useEffect(() => {
+    hotjar.initialize(3483283, 6)
+  }, [])
+
+  return (
+    <div className="main-page-wrapper">
+      <Component {...pageProps} />
+      <ScrollToTop />
+    </div>
+  );
+}
+
